8d156f3dc2e22bdbfb6490bbadba9032cb8ceb70
Cedric Hutchings <cedric@passivelogic.com>
2025-03-27 23:36:17 -0600

    triangle

--- bs/macos_build.sh
+++ bs/macos_build.sh
@@ -1,4 +1,11 @@
+FLAGS="$(head bs/cc_flags.txt)"
+
+if [ -n "$RELEASE" ]; then
+    echo "building in release ..."
+    FLAGS="$(head bs/cc_flags.txt) $(head bs/cc_flags_release.txt)"
+fi
+
 cd build
-cc -o jeu_desprit $(cat ../bs/cc_flags.txt) $(cat ../bs/cc_flags_dev.txt) \
+cc -o jeu_desprit $FLAGS \
    ../src/main.c libGLESv2.dylib `pkg-config sdl3 --cflags --libs` \
    && ./jeu_desprit
--- src/main.c
+++ src/main.c
@@ -22,6 +22,64 @@ int main(int argc, char** argv) {
 
     SDL_GLContext gl = SDL_GL_CreateContext(sdl_window);
 
+    /* shader */
+    GLuint shaderProgram = glCreateProgram();
+    {
+        // Vertex shader
+        const GLchar* vertexSource =
+            "attribute vec4 position;                      \n"
+            "varying vec3 color;                           \n"
+            "void main()                                   \n"
+            "{                                             \n"
+            "    gl_Position = vec4(position.xyz, 1.0);    \n"
+            "    color = gl_Position.xyz + vec3(0.5);      \n"
+            "}                                             \n";
+
+        // Fragment/pixel shader
+        const GLchar* fragmentSource =
+            "precision mediump float;                     \n"
+            "varying vec3 color;                          \n"
+            "void main()                                  \n"
+            "{                                            \n"
+            "    gl_FragColor = vec4 ( color, 1.0 );      \n"
+            "}                                            \n";
+
+        GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexSource, NULL);
+        glCompileShader(vertexShader);
+
+        GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+        glCompileShader(fragmentShader);
+
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        glUseProgram(shaderProgram);
+    }
+
+    /* geometry */
+    {
+        // Create vertex buffer object and copy vertex data into it
+        GLuint vbo;
+        glGenBuffers(1, &vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        GLfloat vertices[] = 
+        {
+             0.0f,  0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f
+        };
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        // Specify the layout of the shader vertex data (positions only, 3 floats)
+        GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+        glEnableVertexAttribArray(posAttrib);
+        glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    }
+
+    glViewport(0, 0, 640, 480);
+
     while (true) {
         SDL_Event event;
         while (SDL_PollEvent(&event) != 0)
@@ -30,6 +88,9 @@ int main(int argc, char** argv) {
 
         glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw the vertex buffer
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         SDL_GL_SwapWindow(sdl_window);
     }
