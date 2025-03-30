7955bca216fef111adcfbc48b4ffc95d88f1185c
Cedric Hutchings <cedric@passivelogic.com>
2025-03-30 14:58:43 -0600

    separate postprocessing shader

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #include <SDL3/SDL_main.h>
 #include "gl_include/gles3.h"
 
+#define jx_COUNT(arr) (sizeof(arr) / sizeof((arr)[0]))
+
 static struct {
   struct {
     SDL_Window    *window;
@@ -16,8 +18,9 @@ static struct {
   size_t window_size_x, window_size_y;
 
   struct {
-    GLuint geo_shader;
-    GLuint tex;
+    struct { GLuint pp, geo; } shader;
+
+    // GLuint tex;
 
     /* resources inside here need to be recreated
      * when the application window is resized. */
@@ -85,13 +88,15 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
       glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT);
 
-      glBindTexture(GL_TEXTURE_2D, jeux.gl.tex);
+      // glBindTexture(GL_TEXTURE_2D, jeux.gl.tex);
+      glUseProgram(jeux.gl.shader.geo);
       glDrawArrays(GL_TRIANGLES, 0, 3);
     }
 
     {
       glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+      glUseProgram(jeux.gl.shader.pp);
       glBindTexture(GL_TEXTURE_2D, jeux.gl.screen.pp_tex);
       glDrawArrays(GL_TRIANGLES, 0, 3);
     }
@@ -122,56 +127,90 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result) {
 static void gl_resize(void);
 static void gl_init(void) {
   /* shader */
-  jeux.gl.geo_shader = glCreateProgram();
   {
-    const GLchar* vs_geo =
-      "attribute vec4 a_pos;                         \n"
-      "varying vec2 v_uv;                            \n"
-      "void main()                                   \n"
-      "{                                             \n"
-      "    gl_Position = vec4(a_pos.xyz, 1.0);       \n"
-      "    v_uv = gl_Position.xy*0.5 + vec2(0.5);    \n"
-      "}                                             \n";
 
-    const GLchar* fs_geo =
-      "precision mediump float;                            \n"
-      "varying vec2 v_uv;                                  \n"
-      "uniform sampler2D u_tex;                            \n"
-      "void main()                                         \n"
-      "{                                                   \n"
-      "    vec4 p = texture2D(u_tex, v_uv);                \n"
-      "    gl_FragColor = p;                               \n"
-      "}                                                   \n";
+    struct {
+      GLuint *dst;
+      const char *debug_name;
+      const GLchar *vs, *fs;
+    } shaders[] = {
+      {
+        .dst = &jeux.gl.shader.geo,
+        .debug_name = "geo",
+        .vs =
+          "attribute vec4 a_pos;                      \n"
+          "varying vec3 v_color;                      \n"
+          "void main()                                \n"
+          "{                                          \n"
+          "    gl_Position = vec4(a_pos.xyz, 1.0);    \n"
+          "    v_color = gl_Position.xyz + vec3(0.5); \n"
+          "}                                          \n"
+        ,
+        .fs =
+          "precision mediump float;               \n"
+          "varying vec3 v_color;                  \n"
+          "uniform sampler2D u_tex;               \n"
+          "void main() {                          \n"
+          "    gl_FragColor = vec4(v_color, 1.0); \n"
+          "}                                      \n"
+      },
+      {
+        .dst = &jeux.gl.shader.pp,
+        .debug_name = "pp",
+        .vs =
+          "attribute vec4 a_pos;                         \n"
+          "varying vec2 v_uv;                            \n"
+          "void main() {                                 \n"
+          "    gl_Position = vec4(a_pos.xyz, 1.0);       \n"
+          "    v_uv = gl_Position.xy*0.5 + vec2(0.5);    \n"
+          "}                                             \n"
+        ,
+        .fs =
+          "precision mediump float;             \n"
+          "varying vec2 v_uv;                   \n"
+          "uniform sampler2D u_tex;             \n"
+          "void main() {                        \n"
+          "    vec4 p = texture2D(u_tex, v_uv); \n"
+          "    gl_FragColor = p;                \n"
+          "}                                    \n"
+      }
+    };
 
-    GLuint vs_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs_shader, 1, &vs_geo, NULL);
-    glCompileShader(vs_shader);
+    for (int shader_index = 0; shader_index < jx_COUNT(shaders); shader_index++) {
 
-    GLuint fs_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs_shader, 1, &fs_geo, NULL);
-    glCompileShader(fs_shader);
+      *shaders[shader_index].dst = glCreateProgram();
 
-    for (int i = 0; i < 2; i++) {
-      GLuint shader = (i == 0) ? vs_shader : fs_shader;
-      char    *name = (i == 0) ? "vs"      : "fs";
-      GLint compiled;
-      glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-      if (compiled != GL_TRUE) {
-        GLint log_length;
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
-        if (log_length > 0) {
-          char *log = malloc(log_length);
-          glGetShaderInfoLog(shader, log_length, &log_length, log);
-          SDL_Log("%s compilation failed: %s\n", name, log);
-          free(log);
+      GLuint vs_shader = glCreateShader(GL_VERTEX_SHADER);
+      glShaderSource(vs_shader, 1, &shaders[shader_index].vs, NULL);
+      glCompileShader(vs_shader);
+
+      GLuint fs_shader = glCreateShader(GL_FRAGMENT_SHADER);
+      glShaderSource(fs_shader, 1, &shaders[shader_index].fs, NULL);
+      glCompileShader(fs_shader);
+
+      for (int i = 0; i < 2; i++) {
+        GLuint shader = (i == 0) ? vs_shader : fs_shader;
+        char  *s_name = (i == 0) ? "vs"      : "fs";
+        GLint compiled;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+        if (compiled != GL_TRUE) {
+          GLint log_length;
+          glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
+          if (log_length > 0) {
+            char *log = malloc(log_length);
+            glGetShaderInfoLog(shader, log_length, &log_length, log);
+            SDL_Log("%s %s compilation failed: %s\n", shaders[shader_index].debug_name, s_name, log);
+            free(log);
+          }
         }
       }
-    }
 
-    glAttachShader(jeux.gl.geo_shader, vs_shader);
-    glAttachShader(jeux.gl.geo_shader, fs_shader);
-    glLinkProgram(jeux.gl.geo_shader);
-    glUseProgram(jeux.gl.geo_shader);
+      GLuint dst = *shaders[shader_index].dst;
+      glAttachShader(dst, vs_shader);
+      glAttachShader(dst, fs_shader);
+      glLinkProgram(dst);
+      glUseProgram(dst);
+    }
   }
 
   /* geometry */
@@ -189,7 +228,7 @@ static void gl_init(void) {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     /* shader data layout */
-    GLint attr_pos = glGetAttribLocation(jeux.gl.geo_shader, "a_pos");
+    GLint attr_pos = glGetAttribLocation(jeux.gl.shader.geo, "a_pos");
     glEnableVertexAttribArray(attr_pos);
     glVertexAttribPointer(attr_pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
   }
@@ -197,37 +236,37 @@ static void gl_init(void) {
   gl_resize();
 
   /* create texture - writes to jeux.gl.tex */
-  {
-     glGenTextures(1, &jeux.gl.tex);
-     glBindTexture(GL_TEXTURE_2D, jeux.gl.tex);
-     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);                             
-     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  // {
+  //    glGenTextures(1, &jeux.gl.tex);
+  //    glBindTexture(GL_TEXTURE_2D, jeux.gl.tex);
+  //    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  //    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  //    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  //    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-     uint8_t data[4 * 4 * 4] = {0};
-     {
-       int i = 0;
-       for (int x = 0; x < 4; x++)
-         for (int y = 0; y < 4; y++)
-           data[i++] = ((x ^ y)%2) ? 0 : 255,
-           data[i++] = ((x ^ y)%2) ? 0 : 255,
-           data[i++] = ((x ^ y)%2) ? 0 : 255,
-           data[i++] = ((x ^ y)%2) ? 0 : 255;
-     }
+  //    uint8_t data[4 * 4 * 4] = {0};
+  //    {
+  //      int i = 0;
+  //      for (int x = 0; x < 4; x++)
+  //        for (int y = 0; y < 4; y++)
+  //          data[i++] = ((x ^ y)%2) ? 0 : 255,
+  //          data[i++] = ((x ^ y)%2) ? 0 : 255,
+  //          data[i++] = ((x ^ y)%2) ? 0 : 255,
+  //          data[i++] = ((x ^ y)%2) ? 0 : 255;
+  //    }
 
-     glTexImage2D(
-       /* GLenum  target         */ GL_TEXTURE_2D,
-       /* GLint   level          */ 0,
-       /* GLint   internalFormat */ GL_RGBA,
-       /* GLsizei width          */ 4,
-       /* GLsizei height         */ 4,
-       /* GLint   border         */ 0,
-       /* GLenum  format         */ GL_RGBA,
-       /* GLenum  type           */ GL_UNSIGNED_BYTE,
-       /* const void *data       */ data
-     );
-  }
+  //    glTexImage2D(
+  //      /* GLenum  target         */ GL_TEXTURE_2D,
+  //      /* GLint   level          */ 0,
+  //      /* GLint   internalFormat */ GL_RGBA,
+  //      /* GLsizei width          */ 4,
+  //      /* GLsizei height         */ 4,
+  //      /* GLint   border         */ 0,
+  //      /* GLenum  format         */ GL_RGBA,
+  //      /* GLenum  type           */ GL_UNSIGNED_BYTE,
+  //      /* const void *data       */ data
+  //    );
+  // }
 }
 
 /* recreates jeux.gl.screen resources to match new jeux.window_size */
